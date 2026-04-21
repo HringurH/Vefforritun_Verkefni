8b6d53bf3aeea73d4c42ebe585fc94bc0ec19cc3
HringurH <hringur.hrafnkelsson@gmail.com>
2026-04-21 10:45:21 +0000

    Lok fyrsta tíma, smávægilegar breytingar gerðar á server.js og byrjað á ejs skjölum

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const menuRoutes = require('./src/routes/menu.routes');
-const infoRoutes = require('./src/routes/info.routes');
 
 const app = express();
 const PORT = 3000;
@@ -11,21 +10,7 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home Page' });
-});
-
-app.use('/matsedill', menuRoutes);
-app.use('/gullfoss', infoRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).render('404', { title: 'Page Not Found' });
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).render('500', { title: 'Server Error' });
-});
+app.use('/', menuRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
